e50a4d3e96a98e80d912195c84c3e01b2563fc68
OliverBWard <OliverBWard4@gmail.com>
2024-09-23 10:16:23 -0400

    added staging area for linear sort

---
+++ staging_Area/main.java
@@ -0,0 +1,20 @@
+package osunram.staging_Area;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int index;
+        int[] arr = { 9, 1, 0, 3, 4 };
+        int lowest = arr[0];
+
+        for (int i = 0; i < arr.length; i++)
+            for (int a = i; i < arr.length; a++) {
+                if (lowest >= arr[0]) {
+
+                }
+            }
+
+    }
+
+}
